eacdc6d2757ee01da0a19f3c722c02cbe3470fcf
jerome13250@gmail.com <jerome@DESKTOP-QTFVOKI>
2020-06-09 16:01:27 +0200

    refactored code to solve sonarLint Cognitive Complexity Code Smell

--- harFileConverter/src/main/java/com/neotys/draft/harfileconverter/HarFileConverterMain.java
+++ harFileConverter/src/main/java/com/neotys/draft/harfileconverter/HarFileConverterMain.java
@@ -16,6 +16,8 @@ import de.sstoehr.harreader.model.HarEntry;
 import de.sstoehr.harreader.model.HarHeader;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,143 +34,173 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * <p>This class converts a HTTP ARCHIVE file (.har) to a 
  * Neoload project format (.nlp). </p>
  * 
- * Use the {@code returnParts()} function to return a List< Part > at Neoload format
- * 
- *
  */
 
 public class HarFileConverterMain {
 
 	static final Logger logger = LoggerFactory.getLogger(HarFileConverterMain.class);
 
+	static Container.Builder actionsContainer = Container.builder().name("Actions");
+	//To avoid server doubles, a list of known servers must be maintained :
+	static List<Server> servers = new ArrayList<>();
 
-	public static void main(String[] args) throws HarReaderException {
+	public static void main(String[] args)  {
 
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("HTTP Archive(.har)", "har");
 		fileChooser.setFileFilter(filter);
-		
+
 		int result = fileChooser.showOpenDialog(fileChooser);
 		if (result == JFileChooser.APPROVE_OPTION) {
 			File harSelectedFile = fileChooser.getSelectedFile();
-			logger.info("Selected file: {}" , harSelectedFile.getAbsolutePath());
-
-
-			Container.Builder actionsContainer = Container.builder().name("Actions");
-			//To avoid server doubles, a list of known servers must be maintained :
-			List<Server> servers = new ArrayList<>();
-
-			HarReader harReader = new HarReader();
-			Har har = harReader.readFromFile(harSelectedFile);
-
-			List<HarEntry> harFileListOfEntries = har.getLog().getEntries();
-			Stream<HarEntry> streamHarEntries = harFileListOfEntries.stream();
-
-			streamHarEntries.forEach( currentHarEntry -> {
-
-				try {
-					URL url = new URL(currentHarEntry.getRequest().getUrl());
-
-					Server server = Server.builder()
-							.name(url.getHost())
-							.host(url.getHost())
-							.port(String.valueOf(url.getPort() != -1 ? url.getPort() : url.getDefaultPort()))
-							.scheme(Server.Scheme.valueOf(url.getProtocol().toUpperCase())) //valueof converts String to equivalent enum value ( HTTP / HTTPS )
-							.build();
-					
-					if(servers.indexOf(server)!=-1) {
-						server = servers.get(servers.indexOf(server));
-					} else {
-						servers.add(server);
-					}
-
-					
-					//Create Stream for HarHeader format:
-					Stream<HarHeader> streamHarHeaders = currentHarEntry.getRequest().getHeaders().stream();
-					//Convert Stream<HarHeader>(de.sstoehr.harreader) to Stream<Header> (Neoload):
-					Stream<Header> streamHeaders = streamHarHeaders.map( currentHarHeader -> 
-						Header.builder()
-						.name(currentHarHeader.getName())
-						.value(currentHarHeader.getValue())
-						.build() 
-					);
-
-					Request.Builder requestBuilder = Request.builder()
-							.name(url.getPath())
-							.url(url.toString())
-							.method(currentHarEntry.getRequest().getMethod().toString())
-							.addAllHeaders(streamHeaders::iterator)
-							.server(url.getHost());
-					
-					
-					
-					//POST data management : body / bodyBinary / parts 
-					//Get the current Content-Type :
-					Optional<String> currentContentType = currentHarEntry.getRequest().getHeaders().stream()
-					.filter(header -> "content-type".equalsIgnoreCase(header.getName()) && header.getValue() != null)
-					.map(HarHeader::getValue)
-					.findFirst()
-					;
-					
-					if (currentContentType.isPresent()  && currentHarEntry.getRequest().getPostData().getText() != null) {
-
-	 					MediaType mediaType = MediaType.parse(currentContentType.get());
-	 					
-	 					//ANY_TEXT_TYPE :
-	 					if(mediaType.is(MediaType.ANY_TEXT_TYPE)) {
-							requestBuilder.body(currentHarEntry.getRequest().getPostData().getText());
-						}
-	 					//FORM_CONTENT:
-						else if("application".equalsIgnoreCase(mediaType.type())
-								&& mediaType.subtype().toLowerCase().contains("form-urlencoded")) { 								
-							requestBuilder.body(currentHarEntry.getRequest().getPostData().getText());
-						}
-						//RAW_CONTENT :
-						else if("application".equalsIgnoreCase(mediaType.type())) {
-							requestBuilder.bodyBinary(currentHarEntry.getRequest().getPostData().getText().getBytes());
-						}
-	 					//MULTIPART_CONTENT:
-						else if("multipart".equalsIgnoreCase(mediaType.type())) {
-							//Get the boundary information in the Content-Type Header:
-							String boundary = ParameterExtractor.extract(currentContentType.get(), "boundary=");
-					        MultipartAnalyzer analyseMultipart = new MultipartAnalyzer(
-					            			currentHarEntry.getRequest().getPostData().getText(),
-					            			boundary);
-					        requestBuilder.parts(analyseMultipart.returnParts());
-						}
-	 					//UNKNOWN Content-Type format :
-						else
-							throw new ContentTypeUnknownException("UNKNOWN Content-Type format : " + currentContentType.get());
-					}
-					Request request = requestBuilder.build();
-					actionsContainer.addSteps(request);
-
-				} catch (Exception e) {
-					logger.error("Failed conversion URL : {} " ,  currentHarEntry.getRequest().getUrl());
-					logger.error("Cause = {} " , e.getMessage());
-				}
-				
-			});
-
-			UserPath userPath = UserPath.builder()
-					.init(Container.builder().name("Init").build())
-					.actions(actionsContainer.build())
-					.end(Container.builder().name("End").build())
-					.name("Demo User Path")
-					.build();
-
-			Project project = Project.builder()
-					.name("test_HARFileConverter_Project")
-					.addUserPaths(userPath)
-					.addServers(servers.toArray(new Server[0]))
-					.build();
-
-			NeoLoadWriter writer = new NeoLoadWriter(project,"C:\\Users\\jerome\\Documents\\NeoLoad Projects");
-			writer.write(true, "7.0", "7.2.2");
 			
-			
-			
+			try {
+				HarFileConverterMain.run(harSelectedFile , "C:\\Users\\jerome\\Documents\\NeoLoad Projects");
+			} catch (HarReaderException e) {
+				logger.error("HAR file conversion failure : {}" ,  harSelectedFile.getName());
+				logger.error("Cause = {} " , e.getMessage());
+			}
+
 		}
 	}
+	
+	/**
+     * Method to run the entire conversion process from har file to Neoload Project
+     *
+     * @param harSelectedFile File HAR File to convert (.har)
+     * @param neoLoadProjectFolder String representing target folder for Neoload project (.nlp)
+	 *
+     */
+
+	public static void run(File harSelectedFile, String neoLoadProjectFolder ) throws HarReaderException {
+
+		logger.info("Selected file: {}" , harSelectedFile.getAbsolutePath());
+
+		HarReader harReader = new HarReader();
+		Har har = harReader.readFromFile(harSelectedFile);
+
+		har.getLog().getEntries().stream().forEach( currentHarEntry -> {
+			try {
+				HarFileConverterMain.buildServer(currentHarEntry);
+				HarFileConverterMain.buildRequest(currentHarEntry);
+
+			} catch (Exception e) {
+				logger.error("Failed conversion URL : {} " ,  currentHarEntry.getRequest().getUrl());
+				logger.error("Cause = {} " , e.getMessage());
+			}
+		});
+
+		UserPath userPath = UserPath.builder()
+				.init(Container.builder().name("Init").build())
+				.actions(actionsContainer.build())
+				.end(Container.builder().name("End").build())
+				.name("Demo User Path")
+				.build();
+
+		Project project = Project.builder()
+				.name("test_HARFileConverter_Project")
+				.addUserPaths(userPath)
+				.addServers(servers.toArray(new Server[0]))
+				.build();
+
+		NeoLoadWriter writer = new NeoLoadWriter(project,neoLoadProjectFolder);
+		writer.write(true, "7.0", "7.2.2");
+
+
+	}
+
+	
+	/**
+     * Method to build a Server Object (Neoload) from a HarEntry Object (har-reader)
+     *
+     * @param currentHarEntry HarEntry
+	 *
+     */
+
+
+	public static void buildServer( HarEntry currentHarEntry ) throws MalformedURLException {
+		URL url = new URL(currentHarEntry.getRequest().getUrl());
+		Server server = Server.builder()
+				.name(url.getHost())
+				.host(url.getHost())
+				.port(String.valueOf(url.getPort() != -1 ? url.getPort() : url.getDefaultPort()))
+				.scheme(Server.Scheme.valueOf(url.getProtocol().toUpperCase())) //valueof converts String to equivalent enum value ( HTTP / HTTPS )
+				.build();
+
+		if(servers.indexOf(server)==-1) {
+			servers.add(server);
+		}
+	}
+
+	/**
+     * Method to build a Request Object (Neoload) from a HarEntry Object (har-reader)
+     *
+     * @param currentHarEntry HarEntry
+	 *
+     */
+	
+	public static void buildRequest( HarEntry currentHarEntry ) throws ContentTypeUnknownException, IOException{
+		URL url = new URL(currentHarEntry.getRequest().getUrl());
+		//Create Stream for HarHeader format:
+		Stream<HarHeader> streamHarHeaders = currentHarEntry.getRequest().getHeaders().stream();
+		//Convert Stream<HarHeader>(de.sstoehr.harreader) to Stream<Header> (Neoload):
+		Stream<Header> streamHeaders = streamHarHeaders.map( currentHarHeader -> 
+		Header.builder()
+		.name(currentHarHeader.getName())
+		.value(currentHarHeader.getValue())
+		.build() 
+				);
+
+		Request.Builder requestBuilder = Request.builder()
+				.name(url.getPath())
+				.url(url.toString())
+				.method(currentHarEntry.getRequest().getMethod().toString())
+				.addAllHeaders(streamHeaders::iterator)
+				.server(url.getHost());
+
+
+		//POST data management : body / bodyBinary / parts 
+		//Get the current Content-Type :
+		Optional<String> currentContentType = currentHarEntry.getRequest().getHeaders().stream()
+				.filter(header -> "content-type".equalsIgnoreCase(header.getName()) && header.getValue() != null)
+				.map(HarHeader::getValue)
+				.findFirst()
+				;
+
+		if (currentContentType.isPresent()  && currentHarEntry.getRequest().getPostData().getText() != null) {
+
+			MediaType mediaType = MediaType.parse(currentContentType.get());
+
+			//ANY_TEXT_TYPE :
+			if(mediaType.is(MediaType.ANY_TEXT_TYPE)) {
+				requestBuilder.body(currentHarEntry.getRequest().getPostData().getText());
+			}
+			//FORM_CONTENT:
+			else if("application".equalsIgnoreCase(mediaType.type())
+					&& mediaType.subtype().toLowerCase().contains("form-urlencoded")) { 								
+				requestBuilder.body(currentHarEntry.getRequest().getPostData().getText());
+			}
+			//RAW_CONTENT :
+			else if("application".equalsIgnoreCase(mediaType.type())) {
+				requestBuilder.bodyBinary(currentHarEntry.getRequest().getPostData().getText().getBytes());
+			}
+			//MULTIPART_CONTENT:
+			else if("multipart".equalsIgnoreCase(mediaType.type())) {
+				//Get the boundary information in the Content-Type Header:
+				String boundary = ParameterExtractor.extract(currentContentType.get(), "boundary=");
+				MultipartAnalyzer analyseMultipart = new MultipartAnalyzer(
+						currentHarEntry.getRequest().getPostData().getText(),
+						boundary);
+				requestBuilder.parts(analyseMultipart.returnParts());
+			}
+			//UNKNOWN Content-Type format :
+			else
+				throw new ContentTypeUnknownException("UNKNOWN Content-Type format : " + currentContentType.get());
+		}
+		Request request = requestBuilder.build();
+		actionsContainer.addSteps(request);
+	}
+
+
 }
